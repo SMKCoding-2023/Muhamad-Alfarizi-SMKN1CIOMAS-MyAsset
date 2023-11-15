b9c4ad96e1d8e6bc28803f039b9098450053692a
DESKTOP-SK1JDHR\ALZEE23 <muhamadalfarizial726@gmail.com>
2023-11-15 07:43:58 +0700

    create project nuxt

--- composables/constants/category.ts
+++ composables/constants/category.ts
@@ -1,22 +1,5 @@
-export const category: Category[] = [
-{
-id: 1,
-name: "Baju",
-},
-{
-id: 2,
-name: "Sepatu",
-},
-{
-id: 3,
-name: "Celana",
-},
-{
-id: 4,
-name: "Jam",
-},
-]
+// category.ts
 export interface Category {
-id? : number,
-name?: string,
+  id?: number;
+  name?: string;
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,4 +5,3 @@
       <Footer />
     </section>
 </template>
-  
